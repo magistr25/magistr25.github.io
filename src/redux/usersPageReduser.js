4ff21f49d78277cba297a19b090de719dc16e9dd
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-04-04 13:15:23 +0600

    Axios. Запросы на сервер(users_API).

--- src/redux/usersPageReduser.js
+++ src/redux/usersPageReduser.js
@@ -30,7 +30,7 @@ export const usersPageReduser = (state = initialState, action) => {
 
             }
         case SET_USERS: {
-                return {...state, users: [...action.users]}
+                return {...state, users: [...state.users,...action.users]}
             }
         default:
             return state
