7c72b8bdafa56accf7ffa0dfdffefcae4273a13c
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-04-04 10:00:26 +0600

    Добавлен компонент <UsersContainer/> в App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {Route, Routes} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
+import UsersContainer from "./components/Users/UsersContainer";
 
 
 
@@ -24,8 +25,11 @@ const App = () => {
                     <Routes>
                         <Route path='/dialogs/*' element = {<DialogsContainer />}/>
                         <Route path='/profile' element = {<Profile />}/>
+
                         <Route path='/music' element = {<Music/>}/>
                         <Route path='/news' element = {<News/>}/>
+
+                        <Route path='/users' element = {<UsersContainer/>}/>
                         <Route path='/settings' element = {<Settings/>}/>
                     </Routes>
                 </div>
