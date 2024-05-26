cfbf4229ee2c24a749d89bb11d862ac32d83f5f3
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-05-26 16:10:40 +0600

    useState, ProfileStatusWithHooks.jsx

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import s from './ProfileInfo.module.css';
 import {Preloader} from "../../common/preloader/Preloader";
-import ProfileStatus from "./ProfileStatus";
+//import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if(!props.profile){
@@ -16,7 +17,7 @@ const ProfileInfo = (props) => {
                 <img src={props.profile.photos.large}/>
 
 
-               <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+               <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             </div>
 
 
---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+const ProfileStatusWithHooks = (props) => {
+
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deActivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status);
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    }
+
+    return (
+        <> {!editMode &&
+            <div>
+                <span onDoubleClick={activateEditMode}>{props.status || "NoStatus"}</span>
+            </div>
+        }
+            {editMode &&
+                <div>
+                    <input onChange={onStatusChange} autoFocus={true} onBlur={deActivateEditMode}
+                           value={status}/>
+                </div>
+            }
+        </>
+    );
+}
+
+
+export default ProfileStatusWithHooks;
