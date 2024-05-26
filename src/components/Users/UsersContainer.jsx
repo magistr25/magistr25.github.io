aba154cf3c72969201e8e12606c8951cd0cbd6fd
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-05-26 10:28:51 +0600

    selectors для странички Users (функции, которые принимают весь state и возвращают только нужные данные).

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -5,11 +5,19 @@ import {
     unfollow,
     setCurrentPage,
     toggleFollowingInProgress,
-    getUsers,
+    requestUsers,
 } from '../../redux/usersPageReducer';
 import {connect} from 'react-redux';
 import {Preloader} from "../common/preloader/Preloader";
 import {compose} from "redux";
+import {
+
+    getCurrentPage,
+    getFollowingInProgress,
+    getIsFetching,
+    getPageSize,
+    getTotalUsersCount, getUsers
+} from "../../redux/users-selectors";
 
 
 
@@ -54,17 +62,20 @@ class UsersContainer extends React.Component {
 }
 
 // _________
+
 const mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress,
+        users: getUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state),
 
 
     }
+
+
 
 // const mapDispatchToProps = (dispatch) => {
 //     return {
@@ -114,5 +125,5 @@ export default compose(
         unfollow,
         setCurrentPage,
         toggleFollowingInProgress,
-        getUsers
+        getUsers: requestUsers
     }))(UsersContainer)
--- src/redux/usersPageReducer.js
+++ src/redux/usersPageReducer.js
@@ -69,11 +69,12 @@ export const setTotalUsersCount = (totalCount) => ({ type: SET_TOTAL_USERS_COUNT
 export const toggleIsFetching = (isFetching) => ({ type: TOGGLE_IS_FETCHING, isFetching });
 export const toggleFollowingInProgress = (isFetching, userId) => ({ type: TOGGLE_IS_FOLLOWING_PROGRESS, isFetching, userId });
 
-export const getUsers = (currentPage, pageSize) => {
+export const requestUsers = (page, pageSize) => {
     return (dispatch) => {
         dispatch(toggleIsFetching(true));
-        usersAPI.getUsers(currentPage, pageSize).then(data => {
-            dispatch(setCurrentPage(currentPage));
+        dispatch(setCurrentPage(page));
+        usersAPI.getUsers(page, pageSize).then(data => {
+
             dispatch(toggleIsFetching(false));
             dispatch(setUsers(data.items));
             dispatch(setTotalUsersCount(data.totalCount));
