1e625e50297f62efb50d2b3505a0305a49059b11
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-05-27 16:35:57 +0600

    shouldComponentUpdate

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 import {Field, reduxForm} from "redux-form";
@@ -26,27 +26,32 @@ let AddNewPostForm = (props) => {
 
 let SuperAddNewPostForm = reduxForm({form: 'ProfileAddNewPostForm'})(AddNewPostForm)
 
-const MyPosts = (props) => {
-
-    let postsElements =
-        props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
-
-    let onAddPost = (values) => {
-        props.addPost(values.newPostText)
+class MyPosts extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps != this.props && ! nextState != this.state;
     }
 
+    render() {
+
+        let postsElements =
+            this.props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+
+        let onAddPost = (values) => {
+            this.props.addPost(values.newPostText)
+        }
 
 
+        return (
 
-    return (
+            < div className={s.postsBlock}>
+                <h3>My posts</h3>
+                <SuperAddNewPostForm onSubmit={onAddPost}/>
+                <div className={s.posts}>
+                    {postsElements}
 
-        < div className={s.postsBlock}>
-            <h3>My posts</h3>
-            <SuperAddNewPostForm onSubmit={onAddPost} />
-            <div className={s.posts}>
-                {postsElements}
-
-            </div>
-        </div>)
+                </div>
+            </div>)
+    }
 }
+
 export default MyPosts
